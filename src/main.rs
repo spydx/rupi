c37754eb5d5bdf368cf5d5f6097980f1c0862bac
Kenneth Fossen <fossen.kenneth@gmail.com>
2022-09-04 16:42:49 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 #![no_std]
 #![no_main]
 
-use core::{panic::PanicInfo, arch::asm};
-
+use core::{arch::asm, panic::PanicInfo};
 
 mod boot {
     use core::arch::global_asm;
 
-    global_asm!(
-        ".section .text._start"
-    );
+    global_asm!(".section .text._start");
 }
 #[allow(non_snake_case)]
 #[no_mangle]
@@ -17,10 +14,10 @@ pub extern "C" fn _start() -> ! {
     let FSELECT = 0x3F20_0008;
     let GPIO_PIN21_SET = 0x3F20001c;
     let GPIO_PIN21_CLEAR = 0x3F20_0028;
-    let SET_OUTPUT = 1<<3;
-    let TURN_ON = 1<<21;
-    let TURN_OFF = 1<<21;
-    
+    let SET_OUTPUT = 1 << 3;
+    let TURN_ON = 1 << 21;
+    let TURN_OFF = 1 << 21;
+
     unsafe {
         core::ptr::write_volatile(FSELECT as *mut i32, SET_OUTPUT);
 
@@ -35,7 +32,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 fn sleep(sec: i32) -> () {
-    let seconds = sec*10000;
+    let seconds = sec * 10000;
 
     unsafe {
         for _ in 1..seconds {
@@ -48,7 +45,6 @@ fn sleep(sec: i32) -> () {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
 
 // 3f20_0008 fsel2 1<<3 turn pin21 into an output
 // 3f20_001c gpiol_set 1<<21 turn pin 21 on
